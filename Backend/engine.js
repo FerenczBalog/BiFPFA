441433590d4ce00dcdd3cb3ce0ec783e95c9a445
Ferencz Balog <balogferencz.artist@gmail.com>
2024-03-21 07:39:24 +0200

    >>> smoothing

--- Backend/engine.js
+++ Backend/engine.js
@@ -1,6 +1,3 @@
-const banner = document.querySelector("#banner")
-const logo = document.querySelector('#logo')
-
 const serv1 = document.querySelector('#serv-1')
 const serv2 = document.querySelector('#serv-2')
 const serv3 = document.querySelector('#serv-3')
@@ -22,13 +19,7 @@ const ProjectTitle3 = document.querySelector("#projectTitle3")
 const ProjectDesc1 = document.querySelector("#projectDesc1")
 const ProjectDesc2 = document.querySelector("#projectDesc2")
 const ProjectDesc3 = document.querySelector("#projectDesc3")
-/*
-const ProjectImg1 = document.querySelector("#projectImg1")
-const ProjectImg2 = document.querySelector("#projectImg2")
-const ProjectImg3 = document.querySelector("#projectImg3")
-*/
 
-const aboutImg = document.querySelector("#despreLogo")
 const aboutDesc = document.querySelector("#despreDesc")
 const aboutDesc2= document.querySelector("#despreDesc2")
 
@@ -37,11 +28,6 @@ fetch('../backend/data.json')
         .then(response => response.json())
         .then(response => {
          // console.log(response);
-
-          /*  Header  < */
-          banner.src = response.logo_transparent;
-          logo.src = response.logo_transparent;
-          /*  Header  > */
 
           /*  Servicii  < */
           serv1.innerHTML = response.serv1;
@@ -71,7 +57,6 @@ fetch('../backend/data.json')
           /*  Proiecte  > */
 
           /*  About   < */
-          aboutImg.src = response.logo_transparent;
           aboutDesc.innerHTML = response.abDesc;
           aboutDesc2.innerHTML = response.abDesc2;
           /*  About   > */
@@ -80,32 +65,3 @@ fetch('../backend/data.json')
     .catch(function(err) {
     console.log('Fetch Error :-S', err);
     });
-
-// SLIDESHOW <
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-// SLIDESHOW >
-
