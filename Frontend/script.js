f259e97a9f584604a5ba90f1035483fab3fff862
Ferencz Balog <balogferencz.artist@gmail.com>
2024-04-15 14:46:23 +0300

    >>> SmallScreenNav update

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,25 +1,23 @@
 const modal = document.querySelector("#nav-modal");
 const openModal = document.querySelector("#bars");
-const closeModal = document.querySelector("#close")
-const overlay = document.querySelector("#overlay");
-const navList = document.querySelector("#nav-bar")
+const closeModal = document.querySelector("#close");
+const navList = document.querySelector("#nav-bar");
 
 //      MODAL <
 openModal.onclick = () => {
-    modal.style.display = "flex"
-    overlay.style.display = "block"
-    navList.style.display = "block"
-    closeModal.style.display = "block"
     openModal.style.display = "none"
+    modal.style.display = "flex"
+    navList.style.display = "block"
+    closeModal.style.display = "block"    
 }
-overlay.onclick = () => {
+closeModal.onclick = () => {
     modal.style.display = "none"
-    overlay.style.display = "none"
     openModal.style.display = "block"
+    closeModal.style.display = "none"
 }
 modal.onclick = () => {
     modal.style.display = "none"
-    overlay.style.display = "none"
-    openModal.style.display = "block"
+    openModal.style.display = "block"    
+    closeModal.style.display = "none"
 }
 //      MODAL >
